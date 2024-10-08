4f924e935da1ab6aaa688324db97a3c73f35deca
Ivko0o <IvelinVasilev91@gmail.com>
2024-10-08 18:10:11 +0300

    New solution

--- hw1_task1.cpp
+++ hw1_task1.cpp
@@ -3,111 +3,46 @@
 
 using namespace std;
 
-void Inclination(double x1, double x2, double y1, double y2, double& inclination);
+void CheckCircleCoordinates(float x, float y, float radius, float& sum_Xsquared_and_Ysquared, float& squaredRadius);
 
 int main()
 {
-	double firstNumberX = 0.0;
-	double secondNumberY = 0.0;
-	double radius = 4.0;
-	double inclinationDiameter = 0;
-	double inclinationRadius = 0;
-	double eps = 1e-4;
-	double x1 = 3.0;
-	double x2 = -3.0;
-	double y1 = 2.646;
-	double y2 = -2.646;
-	double radiusX = 0.0;
-	double radiusY = 0.0;
-
-	
-
-	//Dot B is with coordinates (x = 3 and y = 2,646) - калкулирано с Питагорова теорема
-	double lengthB = sqrt(radius * radius - 3 * 3);
-
-	//Calculates the inclination of the diameter B-D
-	Inclination(x1, x2, y1, y1, inclinationDiameter);
-
-	// Calculates the inclination of the radius O-C
-	Inclination(radiusX, x2, radiusY, y1, inclinationRadius);
-
-	
-	cout << "Enter two numbers (up to 3 numbers after the decimal point) to check in which color field of a given figure they land!" << endl;
-
-	//Takes the input from the user
-	cout << "Enter X coordinate number: ";
-	cin >> firstNumberX;
-	cout << "Enter Y coordinate number: ";
-	cin >> secondNumberY;
-	cout << endl;
+	float inputNumber_X = 0.0;
+	float inputNumber_Y = 0.0;
+	float sum_Xsquared_and_Ysquared = 0.0;
+	float radius = 4.0;
+	float squaredRadius = 0.0;
+	float eps = 1e-4;
 
 
-	double diameterPoint = inclinationDiameter * firstNumberX;
-	
 
-	// If X is bigger than 4 or smalled than -4 it will always be Outside of the field
-	if (firstNumberX >= 4.000|| firstNumberX <= -4.000) {
+	cout << "Enter two float numbers (up to the 3rd digit after the decimal point) so you can see where it lands on the given graph!" << endl;
+	cout << "Enter first number: ";
+	cin >> inputNumber_X;
+	cout << "Enter second number: ";
+	cin >> inputNumber_Y;
+
+
+	CheckCircleCoordinates(inputNumber_X, inputNumber_Y, radius, sum_Xsquared_and_Ysquared, squaredRadius);
+
+
+	// Checks if the dot lands in the graph 
+	if (inputNumber_X > 4 || inputNumber_X < -4) {
 		cout << "Outside!";
 	}
 
-	// Checks if the point is on the diameter B-D line
-	if (abs(secondNumberY - diameterPoint) < eps) {
+	// Checks if the dot is on the circle
+	if (abs(sum_Xsquared_and_Ysquared - squaredRadius) < eps) {
 		cout << "On the edge!";
-		return 0;
 	}
 
 
-	//Checks if the point is on the Y line
-	if (firstNumberX == 0.0){
-		if (secondNumberY <= 6.0 && secondNumberY >= -5.9999) {
-			cout << "On the edge!";
-		}
-	}
-
-	//Checks if the point is on the B-A line
-	if (firstNumberX == 3.0){
-		if (secondNumberY <= 2.646 && secondNumberY >= -2.646) {
-			cout << "On the edge!";
-		}
-		else {
-			cout << "Outside!";
-		}
-	}
-
-	//Checks if the point is on the C-D line
-	if (firstNumberX == -3.0) {
-		if (secondNumberY <= 2.646 && secondNumberY >= -2.646) {
-			cout << "On the edge!";
-		}
-		else {
-			cout << "Outside!";
-		}
-	}
-	
-	//Checks if the point is on the F-I line
-	if (firstNumberX >= -1.0 && firstNumberX <= 1.0) {
-		if (secondNumberY == 5.0) {
-			cout << "On the edge!";
-		}
-	}
-
-	//Checks if its in the grey area or on its edges
-	if (firstNumberX >= -2.0 && firstNumberX <= 2.0) {
-		if (secondNumberY == -6.0 || secondNumberY == -7.0) {
-			cout << "On the edge!";
-
-		}
-		else if (secondNumberY > -7.0 && secondNumberY < -6.0) {
-			cout << "Grey!";
-		}
-	
-	}
 
 }
 
-//Checks the DiameterInclination and rounds it up to the 3rd digit after the decimal point
-void Inclination(double x1, double x2, double y1, double y2, double& inclination) {
-	inclination = (y2 - y1) / (x2 - x1);
-	inclination = (inclination * 1000) / 1000;
+void CheckCircleCoordinates(float x, float y, float radius, float& sum_Xsquared_and_Ysquared, float& squaredRadius) {
+	sum_Xsquared_and_Ysquared = (x * x) + (y * y);
+	squaredRadius = radius * radius;
 }
+
 
